2d954ab9b2568c9beaad6afebda8efa3d9828240
sean <john.kane@kaneleuc.com>
2022-10-02 11:11:28 +1100

    Updated the main.dart example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,5 +1,7 @@
-import 'package:example/test_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_tello/api/enums.dart';
+import 'package:flutter_tello/api/tello_state.dart';
+import 'package:flutter_tello/flutter_tello.dart';
 
 void main() {
   runApp(const MyApp());
@@ -26,6 +28,106 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
       ),
       home: const TestScreen(),
+    );
+  }
+}
+
+class TestScreen extends StatefulWidget {
+  const TestScreen({Key? key}) : super(key: key);
+
+  @override
+  State<TestScreen> createState() => _TestScreenState();
+}
+
+class _TestScreenState extends State<TestScreen> {
+  Tello drone = Tello(configuration: TelloConfiguration());
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+            onPressed: () => drone.setup(),
+            icon: const Icon(Icons.connecting_airports)),
+        title: const Text("Flutter Tello Demo"),
+        actions: [
+          IconButton(
+              onPressed: () => drone.destroy(),
+              icon: const Icon(Icons.disc_full))
+        ],
+      ),
+      body: SafeArea(
+        child: FutureBuilder<Tello>(
+            future: drone.setup(),
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return const Center(child: CircularProgressIndicator());
+              }
+              return ListView(
+                children: [
+                  StreamBuilder<TelloState>(
+                      stream: drone.state,
+                      builder: (BuildContext context, AsyncSnapshot snapshot) {
+                        if (snapshot.hasData) {
+                          return const Text("TEST");
+                        } else if (snapshot.hasError) {
+                          return const Icon(Icons.error_outline);
+                        } else {
+                          return const Center(
+                              child: CircularProgressIndicator());
+                        }
+                      }),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  ListTile(
+                    title: const Text(
+                      "Lift off",
+                    ),
+                    onTap: () => drone.sendCommand(TakeOffCommand()),
+                  ),
+                  ListTile(
+                    title: const Text("Land"),
+                    onTap: () => drone.sendCommand(LandCommand()),
+                  ),
+                  ListTile(
+                    title: const Text("Emergency"),
+                    onTap: () => drone.sendCommand(EmergencyCommand()),
+                  ),
+                  ListTile(
+                    title: const Text("Flip"),
+                    onTap: () =>
+                        drone.sendCommand(FlipCommand(FlipDirection.back)),
+                  ),
+                  ListTile(
+                    title: const Text("Fly To Position"),
+                    onTap: () => drone.sendCommand(FlyToPositionCommand()),
+                  ),
+                  ListTile(
+                    title: const Text("Curve To Position"),
+                    onTap: () => drone.sendCommand(CurveToPositionCommand()),
+                  ),
+                  ListTile(
+                    title: const Text("Rotate Left"),
+                    onTap: () => drone.sendCommand(RotateCommand(
+                        90, RotationalDirection.counter_clockwise)),
+                  ),
+                  ListTile(
+                    title: const Text("Rotate Right"),
+                    onTap: () => drone.sendCommand(
+                        RotateCommand(90, RotationalDirection.clockwise)),
+                  ),
+                  ListTile(
+                    title: const Text("Get Speed"),
+                    onTap: () => drone.sendCommand(GetSpeedCommand()),
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                ],
+              );
+            }),
+      ),
     );
   }
 }
